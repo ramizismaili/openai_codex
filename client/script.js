ee07ba42526d7c8815bd3901762926923de45870
Ramiz Ismaili <rismaili49@gmail.com>
2023-01-06 01:21:57 +0100

    changing fetch data from localhost to server

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server => bots response
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-lfnh.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
